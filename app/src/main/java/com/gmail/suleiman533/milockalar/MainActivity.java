73ca48fc2db7f75be8f49d63eb562723a53bd670
Sueleiman Usman IDRISS <suleiman533@gmail.com>
2018-02-15 09:24:49 +0300

    Add message to Firabase

--- app/src/main/java/com/gmail/suleiman533/milockalar/MainActivity.java
+++ app/src/main/java/com/gmail/suleiman533/milockalar/MainActivity.java
@@ -2,12 +2,32 @@ package com.gmail.suleiman533.milockalar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editMessage;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editMessage = (EditText) findViewById(R.id.editText);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Messages");
+    }
+
+    public void sendMessageClicked (View view){
+        final String messageValue = editMessage.getText().toString().trim();
+        if (!TextUtils.isEmpty(messageValue)){
+            final DatabaseReference newPost = mDatabase.push();
+            newPost.child("Suleiman").setValue(messageValue);
+        }
     }
 }
