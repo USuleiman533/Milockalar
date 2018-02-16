08fff4bdec66776ded73fde8f19a8d52e5970d66
Sueleiman Usman IDRISS <suleiman533@gmail.com>
2018-02-17 00:51:28 +0300

    Get messages from firebase

--- app/src/main/java/com/gmail/suleiman533/milockalar/MainActivity.java
+++ app/src/main/java/com/gmail/suleiman533/milockalar/MainActivity.java
@@ -2,10 +2,13 @@ package com.gmail.suleiman533.milockalar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
-
+import android.widget.TextView;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText editMessage;
     private DatabaseReference mDatabase;
+    private RecyclerView mMessageList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
 
         editMessage = (EditText) findViewById(R.id.editText);
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Messages");
+        mMessageList = (RecyclerView) findViewById(R.id.messageRec);
+        mMessageList.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        mMessageList.setLayoutManager(linearLayoutManager);
     }
 
     public void sendMessageClicked (View view){
@@ -28,6 +37,39 @@ public class MainActivity extends AppCompatActivity {
         if (!TextUtils.isEmpty(messageValue)){
             final DatabaseReference newPost = mDatabase.push();
             newPost.child("Suleiman").setValue(messageValue);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter <Message,MessageViewHolder> FBRA = new FirebaseRecyclerAdapter <Message,MessageViewHolder>(
+
+                Message.class,
+                R.layout.single_message_layout,
+                MessageViewHolder.class,
+                mDatabase
+        ) {
+            @Override
+            protected void populateViewHolder(MessageViewHolder viewHolder, Message model, int position) {
+                viewHolder.setSuleiman(model.getSuleiman());
+            }
+        };
+        mMessageList.setAdapter(FBRA);
+    }
+
+    public static class MessageViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+        public MessageViewHolder (View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setSuleiman(String suleiman){
+            TextView message_suleiman = (TextView) mView.findViewById(R.id.messageText);
+            message_suleiman.setText(suleiman);
         }
     }
 }
--- app/src/main/java/com/gmail/suleiman533/milockalar/Message.java
+++ app/src/main/java/com/gmail/suleiman533/milockalar/Message.java
@@ -5,4 +5,22 @@ package com.gmail.suleiman533.milockalar;
  */
 
 public class Message {
+
+    private String suleiman;
+
+    public Message(){
+
+    }
+
+    public Message(String suleiman){
+        this.suleiman = suleiman;
+    }
+
+    public String getSuleiman(){
+        return suleiman;
+    }
+
+    public void setSuleiman(String suleiman){
+        this.suleiman = suleiman;
+    }
 }
